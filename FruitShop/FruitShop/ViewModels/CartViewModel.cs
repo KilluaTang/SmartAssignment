7642b839d8898e6b697a1989faf5558b446c5e0a
unknown <chopstickster@gmail.com>
2015-04-10 13:20:04 +0200

    update cart

---
+++ FruitShop/FruitShop/ViewModels/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using FruitShop.Models;
+
+namespace FruitShop.ViewModels
+{
+    public class CartViewModel
+    {
+        public IEnumerable<Product> ProductsInCart { get; set; }
+    }
+}
